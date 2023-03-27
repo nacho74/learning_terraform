2bca9f5dd0ad12fdfdbaa888f545104f31804548
Nacho Alvarez <joseignacio.alvarez@oracle.com>
2023-03-27 16:10:39 +0200

    add core volume

--- storage.tf
+++ storage.tf
@@ -9,9 +9,9 @@ resource "oci_core_volume" "test_volume_ac" {
   size_in_gbs = each.value
 }
 
-# resource "oci_core_volume" "my_volume" {
-#   availability_domain = data.oci_identity_availability_domains.my_availability_domains.availability_domains[0]["name"]
-#   compartment_id = var.compartment_ocid
-#   freeform_tags = {"department"= "test"}
-#   display_name = "test-volume"
-# }
+resource "oci_core_volume" "my_volume" {
+  availability_domain = data.oci_identity_availability_domains.my_availability_domains.availability_domains[0]["name"]
+  compartment_id = var.compartment_ocid
+  freeform_tags = {"department"= "test"}
+  display_name = "test-volume"
+}
