d3bd8e5d4ed545d491c3d86c624de7157db10b5c
Nacho Alvarez <38097698+nacho74@users.noreply.github.com>
2023-03-27 13:02:22 +0200

    Update test4.tf

--- test4.tf
+++ test4.tf
@@ -1,17 +1,3 @@
-provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
-}
-
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "region" {}
-variable "compartment_ocid" {}
 variable "block_count" { default = 2 }
 
 data "oci_identity_availability_domains" "my_availability_domains" {
